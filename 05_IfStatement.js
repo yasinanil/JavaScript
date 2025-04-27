d386592735aba21bbf7a310acdb384c919bc4fbd
ANIL <anl@example.com>
2025-04-27 22:28:05 +0300

    Conditional Statements

---
+++ 05_IfStatement.js
@@ -0,0 +1,36 @@
+
+let age = 18;
+
+if (age > 18) {
+    console.log("You are eligible to vote.");
+}
+
+let number = 6;
+if (number % 2 === 0) {
+    console.log("Even number");
+} else {
+    console.log("Odd number");
+}
+
+let score = 69;
+if (score >= 90) {
+    console.log("Grade: A");
+
+} else if (score >= 80) {
+    console.log("Grade: B");
+
+} else if (score >= 70) {
+    console.log("Grade: C");
+
+} else { console.log("Grade: F"); }
+
+let age2 = 17;
+let isCitizen = false;
+if (age2 >= 18) {
+    if (isCitizen) {
+        console.log("You are eligible to vote and apply for the program.");
+    } else {
+        console.log("You are eligible to vote but not for the program.");
+    }
+}
+
---
+++ 07_RetirementApp.js
@@ -0,0 +1,16 @@
+let age = 61;
+let gender = "Female";
+
+if (gender == "Male") {
+    if (age >= 65) {
+        console.log("You are retired.");
+    } else {
+        console.log("You should work.");
+    }
+} else {
+    if (age >= 60) {
+        console.log("You are retired.");
+    } else {
+        console.log("You should work.");
+    }
+}
