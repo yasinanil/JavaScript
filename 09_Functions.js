e8c1e998565852102ea481f8ad50753edfa533c6
ANIL <anl@example.com>
2025-05-01 13:02:23 +0300

    Functions

---
+++ 09_Functions.js
@@ -0,0 +1,20 @@
+//Basic Function (No Parameters, No Return):
+function greet() {
+    console.log("Hello World");
+}
+
+greet();
+
+//Function with Parameters:
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(6, 4);
+
+//Function with Return Value:
+function multiplication(x, y) {
+    return x * y;
+}
+
+console.log(multiplication(6, 5));
