9cb7e0dfb572c52c2c6964fbc97686a2922a2645
ANIL <anl@example.com>
2025-05-03 19:42:28 +0300

    Date Object

---
+++ 14_DateObject.js
@@ -0,0 +1,16 @@
+//Date Object
+const date = new Date();
+console.log(date);
+
+//Getting Date Components
+let year = date.getFullYear();
+console.log(year);
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getDate());
+
+//Getting Time Components
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
